3245f9508681783e1e4f9b57ab78b40dfb328cf4
Jose Luis da Cruz Junior <zejuniortdr@gmail.com>
2013-12-12 10:56:41 +0100

    fieldset no admin

--- localidades/admin.py
+++ localidades/admin.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 from django import forms
 from django.contrib import admin
 from models import *
@@ -13,8 +14,15 @@ class CidadeAdmin(admin.ModelAdmin):
 	search_fields = ('nome', 'uf',)
 	list_display = ('nome', 'estado','lat','lng','capital')
 	list_filter = ['estado','capital',]
-	readonly_fields = ('lat','lng')
-	list_editable = ('capital',)
+	readonly_fields = ('estado','uf','nome','codigo_ibge','gentilico','populacao','area','densidade_demografica','pib','lat','lng','capital',)
+	#list_editable = ('capital',)
 	save_on_top = True
+
+	fieldsets = [
+		('Cidade',{'fields': ['nome','capital',], 'classes': ['wide']}),
+		(u'Estado',{'fields': ['estado', 'uf',], 'classes': ['wide']}),
+		(u'Informacções IBGE (2010)',{'fields': ['codigo_ibge','gentilico','populacao','area','densidade_demografica','pib',], 'classes': ['wide']}),
+		(u'Coordenadas Google Maps',{'fields': ['lat','lng',], 'classes': ['wide']}),
+	]
 
 admin.site.register(Cidade, CidadeAdmin)
